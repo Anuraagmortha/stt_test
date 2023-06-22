ca9289d635e7cf621966885e468d726b33f7609e
Anuraagmortha <108216342+Anuraagmortha@users.noreply.github.com>
2023-06-22 19:51:36 +0530

    Add files via upload

---
+++ test_conformer.py
@@ -0,0 +1,99 @@
+
+import riva.client
+import os
+from datetime import datetime
+from datetime import date
+import pandas as pd
+from evaluate import load
+#jiwer is also used as a dependency
+
+
+RIVA_HOST  = '71.178.43.144:6605'
+CSV_FILE = ""
+SAMPLE = 50
+FILE_DIR = 'D:/sp/stt_test/riva_test/chunk-en-2023-5-22/'
+
+positive_boost_words =['mutual', 'fund', 'prudential', 'investment', 'icici', 'scheme', 'sip', 'time', 'right', 'number', 'thank', 'morning', 'afternoon', 'funds', 'speaking', 'website', 'amount', 'market', 'actually', 'may', 'related', 'help', 'read', 'documents', 'need', 'carefully', 'option', 'rupees', 'trying', 'redemption', 'charges', 'risk', 'request', 'day', 'invest', 'completed', 'form', 'speak', 'given', 'please', 'dot', 'term', 'process', 'regarding', 'investments', 'subject', 'check', 'transaction', 'hundred', 'observed', 'date', 'inform', 'send', 'folio', 'login', 'visited', 'account', 'details', 'service', 'october', 'application', 'evening', 'subjected', 'month', 'last', 'years', 'received', 'mail', 'new', 'audible', 'means', 'successfully', 'period', 'problem', 'password', 'redeem', 'register', 'information', 'email', 'app', 'invested', 'valuable', 'holding', 'registered', 'year', 'thirty', 'bank', 'issue', 'mention', 'procedure', 'visit', 'equity', 'user', 'manager', 'online', 'banking', 'point', 'contact', 'kumar', 'complete']
+positive_lm_score = 100.0
+negative_boost_words = ["find"]
+negative_lm_score = -100.0
+def get_encoding(audio_file):
+    file_extension = audio_file.split('.')[-1]
+    if file_extension == 'wav':
+        encoding = riva.client.AudioEncoding.LINEAR_PCM
+    elif file_extension == 'flac':
+        encoding = riva.client.AudioEncoding.FLAC
+    elif file_extension == 'alaw':
+        encoding = riva.client.AudioEncoding.ALAW
+    elif file_extension == 'mulaw':
+        encoding = riva.client.AudioEncoding.MULAW
+    else:
+        raise Exception(f'Audio format ".{file_extension}" not supported.')
+    return encoding
+def run_inference(audio_file, server=RIVA_HOST, print_full_response=False):
+    with open(audio_file, 'rb') as fh:
+        data = fh.read()
+    auth = riva.client.Auth(uri=server)
+    client = riva.client.ASRService(auth)
+    config = riva.client.RecognitionConfig(
+        encoding=get_encoding(audio_file),
+        language_code="en-US",
+        max_alternatives=1,
+        enable_automatic_punctuation=False,
+    )
+    riva.client.add_word_boosting_to_config(config, positive_boost_words, positive_lm_score)
+    riva.client.add_word_boosting_to_config(config, negative_boost_words, negative_lm_score)
+    riva.client.add_audio_file_specs_to_config(config, audio_file)
+    response = client.offline_recognize(data, config)
+    if print_full_response:
+        return response
+    else:
+        return response.results[0].alternatives[0].transcript
+
+
+def calculate_wer():
+    df_first = pd.read_csv('D:/sp/stt_test/riva_test/ChunkedCallsData-en-2023-05-22.csv')
+    df_first['format_in_wav'] = df_first['filename'].str.replace('mp3', 'wav')
+    df_sample = pd.DataFrame()
+    df_sample = df_first.sample(SAMPLE)
+    reference_text_list = df_sample['modeified_text'].to_list()
+    predicted_text_list = []
+    for audio in df_sample['format_in_wav']:
+        full_audio = FILE_DIR + audio
+        print(full_audio)
+        predicted_text_value = run_inference(full_audio, server=RIVA_HOST, print_full_response=False)
+        predicted_text_list.append(predicted_text_value)
+    
+    print("reference text length = ", len(reference_text_list), "predicted text length = ", len(predicted_text_list))
+    wer = load("wer")
+    wer_score = wer.compute(references=reference_text_list, predictions=predicted_text_list)
+    print("wer score = ", wer_score)
+    return wer_score
+
+
+def main():
+    #df = pd.read_csv(CSV_FILE)
+    wer_value = calculate_wer()
+    report_dict = {
+        'wer' : wer_value,
+        'sample' : SAMPLE,
+        'client_project' : 'amc_english_audio'
+    }
+
+    report_df = pd.DataFrame(list(report_dict))
+
+    current_date = date.today()
+    folder_name = current_date.strftime("%Y-%m-%d")
+    folder_path = 'D:/sp/stt_test/reports/'
+    os.makedirs(os.path.join(folder_path, folder_name), exist_ok=True)
+
+    current_datetime = datetime.now()
+    datetime_string = current_datetime.strftime("%Y-%m-%d_%H-%M-%S")
+
+    file_name = f"report_{datetime_string}.csv"
+    file_path = os.path.join(folder_path + "/" + folder_name, file_name)
+    report_df.to_csv(file_path)
+
+
+if __name__ == "__main__":
+    main()
